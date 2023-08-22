5c69dbe83a5e3b0ae633ce61b95412ee40925f6c
David <wjw3298@gmail.com>
2023-08-22 14:31:51 -0400

    complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
-function App() {
 
-  // replace 'false' with a state variable that can be toggled between true and false
-  // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
+function App() {
+  const [isOn, lightIsOn] = useState(true)
+  // console.log({isOn, lightIsOn})
+
+
+  function handleSwitch(){
+    lightIsOn(!isOn)
+  }
+  
+  
+  const appClass = isOn ? "App dark" : "App light"
 
   return (
     <div className={appClass}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button onClick={handleSwitch}> Dark Mode</button>
       </header>
       <ShoppingList items={itemData} />
     </div>
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+
+  const [inCart, addToCart] = useState(false)
+  // console.log(inCart, addToCart)
+
+  function handleAddToCart(){
+    addToCart(!inCart)
+  }
+
+ const add = inCart ? "Remove From Cart" : "Add to Cart"
+
+
+
   return (
     <li className="">
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className='add' onClick={handleAddToCart}>{add}</button>
     </li>
   );
 }
